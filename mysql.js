088668b221ea1d7b04b64b197fc47e2b86d128ed
Michael Wood <michaelwood33311@icloud.com>
2022-12-19 11:12:40 -0500

    added mysql connection library

---
+++ mysql.js
@@ -0,0 +1,34 @@
+const mysqlDb = require('mysql');
+const require('dotenv').config();
+
+const pool = mysqlDb.createPool({
+  connectionLimit: 2,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
+exports.sqlReady = false;
+exports.mysql = mysqlDb;
+
+pool.getConnection((err,connection) => {
+  if(err)
+  throw err;
+  console.log('Database connected successfully');
+  connection.release();
+  this.sqlReady = true;
+});
+
+exports.sqlQuery = query => {
+  return new Promise((resolve, reject) => {
+    pool.query(query, (err, data) => {
+      if(err) {
+        reject(err);
+        return;
+      }
+      resolve (data);
+    })
+  })
+}
+

2d4e4a181a71b49f885f7c30b05d966865f1788b
Michael Wood <michaelwood33311@icloud.com>
2022-12-19 10:14:14 -0500

    added video query to pixabay library

--- pixabay.js
+++ pixabay.js
@@ -1,14 +1,40 @@
 const axios = require('axios');
 require('dotenv').config();
 
-exports.query = async (query, page = 1) => {
+exports.imageQuery = async (query, page = 1) => {
     const request = {
         url: 'https://pixabay.com/api/',
         method: 'get',
         params: {
             q: query,
             orientation: "horizontal",
-            per_page: 100,
+            per_page: 200,
+            page,
+            key: process.env.PIXABAY_KEY
+        },
+        headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+    }
+
+    let data;
+
+    try {
+        let response = await axios(request);
+        data = response.data;
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
+
+    console.log(data);
+}
+
+exports.videoQuery = async (query, page = 1) => {
+    const request = {
+        url: 'https://pixabay.com/api/videos/',
+        method: 'get',
+        params: {
+            q: query,
+            per_page: 200,
             page,
             key: process.env.PIXABAY_KEY
         },
